21cea057e106372e1eb83fdfc584990ff8680f4b
hanxianzhe1116 <876605943@qq.com>
2020-02-14 17:08:25 +0800

    Variable变量

--- DeepLearning_test/DL_test3.py
+++ DeepLearning_test/DL_test3.py
@@ -1,12 +1,15 @@
+'''
+    Variable变量
+'''
 import tensorflow as tf
 
-state = tf.Variable(0, name='counter')
+state = tf.Variable(0, name='counter')  # 变量state={name:counter, value:0}
 print(state.name)
-one = tf.constant(1)
+one = tf.constant(1)  # 常量 1
 
 new_value = tf.add(state, one)
-update = tf.assign(state, new_value)
-
+update = tf.assign(state, new_value)  # 将new_value分配给state
+# 若定义了Variable，则需要初始化
 init = tf.initialize_all_variables()  # must have if define variable
 
 with tf.Session() as sess:
